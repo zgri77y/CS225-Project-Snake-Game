3f96d8b6a38973c8622ae58dae2e9ff08b22bfa7
zgri77y <42725093+zgri77y@users.noreply.github.com>
2021-11-05 12:33:19 -0700

    Update Start.cpp

--- Start.cpp
+++ Start.cpp
@@ -27,7 +27,7 @@ class Apple {
 private:
 	int apple_x = 10;
 	int apple_y = 10;
-
+	int score = 0;
 public:
 
 	int getApple_X() {
@@ -48,21 +48,43 @@ public:
 		apple_y = rand() % 18 + 1;
 
 	};
+
+	void AddScore() {
+		score++;
+	}
+
+	int GetScore() {
+		return score;
+	}
 };
 
+class Snake {
+private:
+	int Move = 0;
+	bool Up, Down, Left, Right;
+	int x = 6;
+	int y = 6;
+public:
+	Snake();
+	void MoveUp();
+	void MoveDown();
+	void MoveLeft();
+	void MoveRight();
+	int GetX() { return x; }
+	int GetY() { return y; }
+	int GetMove() { return Move; }
+};
 
+char Direction[256];
+void board(Apple& a1, Snake& CPPSnake);
 
-int x = 6;
-int y = 6;
-int nmin = 0;
-int score = 0;
-
-void board(Apple& a1);
 
 int main() {
 	bool Game_Runnning;
 	int input;
 	Apple a1;
+	Snake CPPSnake;
+	int score;
 
 	cout << "Press any button to start!";
 	_getch();
@@ -71,48 +93,29 @@ int main() {
 
 	srand(time(NULL));
 
-	board(a1);
+	board(a1, CPPSnake);
 
 	while (Game_Runnning == true) {
 		input = _getch();
 
 		switch (input) {
 		case 'w':
-			if (y == nmin) {
-				break;
-			}
-			else {
-				y = y - 1;
-				break;
-			}
+			CPPSnake.MoveUp();
+			break;
 		case 'a':
-			if (x == nmin) {
-				break;
-			}
-			else {
-				x = x - 1;
-				break;
-			}
+			CPPSnake.MoveLeft();
+			break;
 		case 's':
-			if (y == 14) {
-				break;
-			}
-			else {
-				y = y + 1;
-				break;
-			}
+			CPPSnake.MoveDown();
+			break;
 		case 'd':
-			if (x == 19) {
-				break;
-			}
-			else {
-				x = x + 1;
-				break;
-			}
+			CPPSnake.MoveRight();
+			break;
 		}
 
-		board(a1);
+		board(a1, CPPSnake);
 		ofstream fout("Results.txt");
+		score = a1.GetScore();
 		fout << "Your score is: " << score << endl;
 		fout.close();
 
@@ -120,35 +123,145 @@ int main() {
 
 }
 
-void board(Apple& a1) {
+void board(Apple& a1, Snake& CPPSnake) {
 	system("CLS");
 
+	int x = CPPSnake.GetX();
+	int y = CPPSnake.GetY();
+	int Move = CPPSnake.GetMove();
+
+	bool prB = false;
+
+	int score = a1.GetScore();
 	cout << "Score: " << score << endl;
+
+	int BdX[100];
+	int BdY[100];
+
+	BdX[0] = x;
+	BdY[0] = y;
+
+	for (int k = 1; k < score+1; k++) {
+		switch (Direction[Move - k]) {
+		case 'U':
+			BdY[k] = BdY[k - 1] + 1;
+			BdX[k] = BdX[k - 1];
+			break;
+		case 'D':
+			BdY[k] = BdY[k - 1] - 1;
+			BdX[k] = BdX[k - 1];
+			break;
+		case 'L':
+			BdX[k] = BdX[k - 1] + 1;
+			BdY[k] = BdY[k - 1];
+			break;
+		case 'R':
+			BdX[k] = BdX[k - 1] - 1;
+			BdY[k] = BdY[k - 1];
+			break;
+		}
+	}
 
 	cout << "||||||||||||||||||||||" << endl;
 	for (int i = 0; i < 15; i++) {
 		cout << '|';
 		for (int j = 0; j < 20; j++) {
-			if (i == y && j == x) {
+
+			for (int l = 1; l < score + 1; l++) {
+				if (i == BdY[l] && j == BdX[l]) {
+					cout << 'B';
+					prB = true;
+				}
+			}
+
+			if (i == BdY[0] && j == BdX[0] && prB == false) {
 				cout << 'S';
 			}
-			else if (i == a1.getApple_X() && j == a1.getApple_Y()) {
+			else if (i == a1.getApple_X() && j == a1.getApple_Y() && prB == false) {
 				cout << "O";
 
 			}
-
-			else {
+			else if (prB == false) {
 				cout << ' ';
 			}
+
+			prB = false;
 
 		}
 		cout << '|' << endl;
 	}
+
 	cout << "||||||||||||||||||||||" << endl;
+
 	if ((a1.getApple_X() == y) && (a1.getApple_Y() == x)) {
 		cout << "You ate the apple" << endl;
-		score++;
+		a1.AddScore();
 		a1.appleLocation();
+	}
+
+}
+
+Snake::Snake() {
+	Up = false;
+	Down = true;
+	Left = false;
+	Right = false;
+
+	x = 6;
+	y = 6;
+}
+
+void Snake::MoveUp() {
+	if (y > 0 && Down == false) {
+		y = y - 1;
+		Move++;
+		Direction[Move] = 'U';
+
+		Up = true;
+		Down = false;
+		Left = false;
+		Right = false;
+	}
+}
+
+void Snake::MoveDown() {
+	if (y < 14 && Up == false) {
+		y = y + 1;
+		Move++;
+		Direction[Move] = 'D';
+
+		Up = false;
+		Down = true;
+		Left = false;
+		Right = false;
+	}
+
+}
+
+void Snake::MoveLeft() {
+	if (x > 0 && Right == false) {
+		x = x - 1;
+		Move++;
+		Direction[Move] = 'L';
+
+		Up = false;
+		Down = false;
+		Left = true;
+		Right = false;
+	}
+
+}
+
+void Snake::MoveRight() {
+	if (x < 19 && Left == false) {
+		x = x + 1;
+		Move++;
+		Direction[Move] = 'R';
+
+		Up = false;
+		Down = false;
+		Left = false;
+		Right = true;
 	}
 
 }
